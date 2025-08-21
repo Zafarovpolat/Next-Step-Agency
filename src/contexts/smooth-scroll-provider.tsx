c6de99698665ed79fbe421ad3724a7fc60fb021f
Polat Zafarov <atuin59354081@gmail.com>
2025-08-21 17:16:49 +0000

    fix scroll pace

--- src/contexts/smooth-scroll-provider.tsx
+++ src/contexts/smooth-scroll-provider.tsx
@@ -5,7 +5,9 @@ import Lenis from '@studio-freight/lenis'
 
 export function SmoothScrollProvider({ children }: { children: ReactNode }) {
   useEffect(() => {
-    const lenis = new Lenis()
+    const lenis = new Lenis({
+      lerp: 0.07,
+    })
 
     function raf(time: number) {
       lenis.raf(time)
